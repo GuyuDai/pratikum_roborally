7b811cba9c90261618d1e72f906ee35bd1651c7e
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-06-05 15:56:47 +0200

    Added reboot

--- src/main/java/server/Player/Robot.java
+++ src/main/java/server/Player/Robot.java
@@ -57,5 +57,23 @@ public class Robot extends BoardElem implements RobotAction {
       moveOneStep();
     }
   }
+
+  /** @author Niklas
+   * Reboot action: Take two SPAM damage cards, and place them in your discard pile.
+   * Regardless of the current register, your programming is canceled
+   * for the round, and you may not complete remaining registers.
+   * Immediately discard any programming cards (including damage cards) in your remaining registers,
+   * as well as those in your hand.
+   * You must wait until the next round to program your robot.
+   * Place your robot on the reboot token that is on the same board where your robot was when it rebooted.
+   * You may turn your robot to face any direction.
+   * If you rebooted from the start board, place your robot on the space where you started the game.
+   */
+  public void reboot(){
+    discardPile.add(new Spam());
+    discardPile.add(new Spam());
+    discardPile.removeAll();
+    setPosition(rebootToken);
+  }
 }
 
