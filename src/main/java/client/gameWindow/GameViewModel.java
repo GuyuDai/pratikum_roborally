d28d34baf91c932b44b7d9565f623c1be842fc8d
Felicia Saruba <felicia.saruba@gmx.de>
2022-06-27 19:40:01 +0200

    printing card on game window, moving resources

--- src/main/java/client/gameWindow/GameViewModel.java
+++ src/main/java/client/gameWindow/GameViewModel.java
@@ -7,16 +7,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
+import server.CardTypes.*;
 import transfer.Player;
 import transfer.request.Message;
 import transfer.request.MessageTypes;
 import transfer.request.RequestType;
 import transfer.request.RequestWrapper;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.Parent;
@@ -30,7 +37,7 @@ import javafx.scene.control.TextField;
 /**
  * @author Nargess Ahmadi, Felicia Saruba
  */
-public class GameViewModel  {
+public class GameViewModel {
 
 
     //public GameViewModel(){
@@ -99,6 +106,8 @@ public class GameViewModel  {
     private TilePane discardDeck;
     @FXML
     private TilePane hand;
+    @FXML
+    private Button playCardBtn;
 
     public String window = "Game";
 
@@ -106,7 +115,9 @@ public class GameViewModel  {
     public void initialize() {
         list.itemsProperty().set(model.getListContentProperty());
         input.textProperty().bindBidirectional(model.getTextFieldContent());
+        hboxHand.getChildren().add(new ImageView());
     }
+
 
     public void setNodeElements(VBox container, ListView<String> list, TextField input, Button sendBtn) {
         this.gameContainer = gameContainer;
@@ -114,10 +125,12 @@ public class GameViewModel  {
         this.list = list;
         this.input = input;
         this.sendBtn = sendBtn;
+        this.hboxHand = hboxHand;
+
         //this.currentPlayer = currentPlayer;
     }
 
-    public static void setCurrentPlayer(Player player){
+    public static void setCurrentPlayer(Player player) {
         currentPlayer = player;
     }
 
@@ -151,11 +164,11 @@ public class GameViewModel  {
     /**
      * checks if it is a direct message or a message for all
      */
-    private void checkInput(String message){
+    private void checkInput(String message) {
         String sendableRequest = "";
 
 
-        if(message.startsWith("@")) {
+        if (message.startsWith("@")) {
             sendableRequest = createDirectMessage(message);
 
         /*
@@ -166,7 +179,7 @@ public class GameViewModel  {
         } else {
             sendableRequest = createMessage(message);
         }
-        if(!sendableRequest.isEmpty()){
+        if (!sendableRequest.isEmpty()) {
             try {
                 Client.getClientReceive().getWriteOutput().write(sendableRequest);
                 Client.getClientReceive().getWriteOutput().newLine();
@@ -178,27 +191,27 @@ public class GameViewModel  {
     }
 
 
-    private String createMessage(String message){
+    private String createMessage(String message) {
 
         String createMessageWrapped = gson.toJson(new RequestWrapper(new Message(currentPlayer.getName(), message, MessageTypes.CLIENT_MESSAGE), RequestType.MESSAGE));
         return createMessageWrapped;
     }
 
-    private String createDirectMessage(String message){
+    private String createDirectMessage(String message) {
         message = message.replace("@", "");
-        String [] splittingTarget = message.split(" ");
+        String[] splittingTarget = message.split(" ");
         StringBuilder realMessage = new StringBuilder("");
-        for(int i = 1; i < splittingTarget.length; i++){
+        for (int i = 1; i < splittingTarget.length; i++) {
             realMessage.append(splittingTarget[i]).append(" ");
         }
-        String createMessageWrapped = gson.toJson(new RequestWrapper(new Message("Private Message from "+ currentPlayer.getName(), splittingTarget[0].trim(), realMessage.toString().trim(), MessageTypes.PRIVATE_MESSAGE), RequestType.MESSAGE));
+        String createMessageWrapped = gson.toJson(new RequestWrapper(new Message("Private Message from " + currentPlayer.getName(), splittingTarget[0].trim(), realMessage.toString().trim(), MessageTypes.PRIVATE_MESSAGE), RequestType.MESSAGE));
         return createMessageWrapped;
     }
 
 
     //send messages using keyboard "Enter" key
     @FXML
-    public void keyboardAction(KeyEvent keyEvent) throws IOException{
+    public void keyboardAction(KeyEvent keyEvent) throws IOException {
         if (keyEvent.getCode() == KeyCode.ENTER) {
             sendButtonAction(null);
         }
@@ -206,9 +219,10 @@ public class GameViewModel  {
 
     /**
      * opens ChatWindow by clicking the Open Chat Button
+     *
      * @param actionEvent
      */
-    public void chatBtnAction(ActionEvent actionEvent){
+    public void chatBtnAction(ActionEvent actionEvent) {
         if (container.isVisible()) {
             container.setVisible(false);
             chatBtn.setText("Open Chat");
@@ -229,9 +243,123 @@ public class GameViewModel  {
             stageLobby.setTitle("Lobby");
             stageLobby.setScene(new Scene(rootMap));
             stageLobby.show();
-        } catch (Exception e){
+        } catch (Exception e) {
         }
         LobbyViewModel.setWindowName("Lobby");
     }
+
+    ArrayList<Card> nineCardsFromServer = new ArrayList<>();
+    ArrayList<Card> discardPile;
+
+    public void printCards() {
+        nineCardsFromServer.add(new MoveOne());
+        nineCardsFromServer.add(new MoveOne());
+        nineCardsFromServer.add(new MoveOne());
+        nineCardsFromServer.add(new MoveOne());
+        nineCardsFromServer.add(new MoveOne());     // 5 x Move One
+
+        nineCardsFromServer.add(new MoveTwo());
+        nineCardsFromServer.add(new MoveTwo());
+        nineCardsFromServer.add(new MoveTwo());     // 3 x Move Two
+
+        nineCardsFromServer.add(new MoveThree());   // 1 x Move Three
+
+        nineCardsFromServer.add(new Again());
+        nineCardsFromServer.add(new Again());       // 2 x Again
+
+        nineCardsFromServer.add(new BackUp());      // 1 x BackUp
+
+        nineCardsFromServer.add(new TurnLeft());
+        nineCardsFromServer.add(new TurnLeft());
+        nineCardsFromServer.add(new TurnLeft());    // 3 x Turn Left
+
+        nineCardsFromServer.add(new TurnRight());
+        nineCardsFromServer.add(new TurnRight());
+        nineCardsFromServer.add(new TurnRight());   // 3 x Turn Right
+
+        nineCardsFromServer.add(new UTurn());       // 1 x UTurn
+
+        nineCardsFromServer.add(new PowerUp());     // 1 x PowerUp
+
+        Collections.shuffle(nineCardsFromServer);
+
+
+        URL url1 = getClass().getResource("/programmingCards/move1.png");
+        if (url1 == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image image1 = new Image(url1.toString());
+        hand1.setImage(image1);
+
+
+        URL url2 = getClass().getResource("/programmingCards/move2.png");
+        if (url2 == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image image2 = new Image(url2.toString());
+        hand2.setImage(image2);
+
+
+        URL urlMove3 = getClass().getResource("/programmingCards/move3.png");
+        if (urlMove3 == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageMove3 = new Image(urlMove3.toString());
+        hand3.setImage(imageMove3);
+
+
+        URL urlAgain = getClass().getResource("/programmingCards/again.png");
+        if (urlAgain == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageAgain = new Image(urlAgain.toString());
+        hand4.setImage(imageAgain);
+
+
+        URL urlBackUp = getClass().getResource("/programmingCards/backUp.png");
+        if (urlBackUp == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageBackUp = new Image(urlBackUp.toString());
+        hand5.setImage(imageBackUp);
+
+
+        URL urlTurnLeft = getClass().getResource("/programmingCards/turnLeft.png");
+        if (urlTurnLeft == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageBTurnLeft = new Image(urlTurnLeft.toString());
+        hand6.setImage(imageBTurnLeft);
+
+
+        URL urlTurnRight = getClass().getResource("/programmingCards/turnRight.png");
+        if (urlTurnRight == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageBTurnRight = new Image(urlTurnRight.toString());
+        hand7.setImage(imageBTurnRight);
+
+
+        URL urlUTurn = getClass().getResource("/programmingCards/uTurn.png");
+        if (urlUTurn == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imageUTurn = new Image(urlUTurn.toString());
+        hand8.setImage(imageUTurn);
+
+
+        URL urlPowerUp = getClass().getResource("/programmingCards/powerUp.png");
+        if (urlPowerUp == null) {
+            throw new IllegalArgumentException("File not found: /programmingCards/" + nineCardsFromServer.get(0).getCardName());
+        }
+        Image imagePowerUp = new Image(urlPowerUp.toString());
+        hand9.setImage(imagePowerUp);
+
+    }
+
+    public void showCardBtnAction() {
+        printCards();
+    }
+
 
 }
