65cbf1d475d40a909658c290d5fdc9597b095216
Felicia Saruba <felicia.saruba@gmx.de>
2022-06-28 19:36:50 +0200

    Map Builder

--- src/main/java/client/MapBuilder.java
+++ src/main/java/client/MapBuilder.java
@@ -6,6 +6,9 @@ import java.net.URL;
 
 public class MapBuilder {
 
+    /**
+     * BoardImages
+     */
     URL startPoint = getClass().getResource("BoardImages/StartPoint.png");
     Image imageStartPoint = new Image(startPoint.toString());
 
@@ -15,6 +18,10 @@ public class MapBuilder {
     URL wallEcke = getClass().getResource("BoardImages/WallEcke.png");
     Image imageWallEcke = new Image(wallEcke.toString());
 
+
+    /**
+     * Checkpoints
+     */
     URL checkPoint1 = getClass().getResource("Checkpoints/Checkpoint1.png");
     Image imageCheckPoint1 = new Image(checkPoint1.toString());
 
@@ -33,6 +40,10 @@ public class MapBuilder {
     URL checkPoint6 = getClass().getResource("Checkpoints/Checkpoint6.png");
     Image imageCheckPoint6 = new Image(checkPoint6.toString());
 
+
+    /**
+     * ConveyorBelts
+     */
     URL blueBeltBottom = getClass().getResource("ConveyorBelts/BlueBeltBottom.png");
     Image imageBlueBeltBottom = new Image(blueBeltBottom.toString());
 
@@ -48,7 +59,6 @@ public class MapBuilder {
     URL blueRotating = getClass().getResource("ConveyorBelts/blueRotating.png");
     Image imageBlueRotating = new Image(blueRotating.toString());
 
-
     URL greenBeltBottom = getClass().getResource("ConveyorBelts/GreenBeltBottom.png");
     Image imageGreenBeltBottom = new Image(greenBeltBottom.toString());
 
@@ -63,6 +73,64 @@ public class MapBuilder {
 
     URL greenRotating = getClass().getResource("ConveyorBelts/GreenRotating.png");
     Image imageGreenRotating = new Image(greenRotating.toString());
+
+    URL rbBottom = getClass().getResource("ConveyorBelts/RB Bottom.png");
+    Image imageRBBottom = new Image(rbBottom.toString());
+
+    URL rbBottom2 = getClass().getResource("ConveyorBelts/RB Bottom 2.png");
+    Image imageRBBottom2 = new Image(rbBottom2.toString());
+
+    URL rbLeft = getClass().getResource("ConveyorBelts/RB left.png");
+    Image imageRBLeft = new Image(rbLeft.toString());
+
+    URL rbLeft2 = getClass().getResource("ConveyorBelts/RB left2.png");
+    Image imageRBLeft2 = new Image(rbLeft2.toString());
+
+    URL rbRight = getClass().getResource("ConveyorBelts/RB right.png");
+    Image imageRBRight = new Image(rbRight.toString());
+
+    URL rbRight2 = getClass().getResource("ConveyorBelts/RB right2.png");
+    Image imageRBRight2 = new Image(rbRight2.toString());
+
+    URL rbTop = getClass().getResource("ConveyorBelts/RB top.png");
+    Image imageRBTop = new Image(rbTop.toString());
+
+    URL rbTop2 = getClass().getResource("ConveyorBelts/RB top2.png");
+    Image imageRBTop2 = new Image(rbTop2.toString());
+
+
+    /**
+     * EnergyImg
+     */
+    URL energyOff = getClass().getResource("EnergyImg/energyOff.png");
+    Image imageenergyOff = new Image(energyOff.toString());
+
+    URL energyOn = getClass().getResource("EnergyImg/energyOn.png");
+    Image imageenergyOn = new Image(energyOn.toString());
+
+
+    /**
+     * Laser
+     */
+    URL laser1 = getClass().getResource("Laser/Laser1.png");
+    Image imagelaser1 = new Image(laser1.toString());
+
+    URL laser2 = getClass().getResource("Laser/Laser2.png");
+    Image imagelaser2 = new Image(laser2.toString());
+
+    URL laser3 = getClass().getResource("Laser/Laser3.png");
+    Image imagelaser3 = new Image(laser3.toString());
+
+    URL wallLaser1 = getClass().getResource("Laser/WallLaser1.png");
+    Image imagewallLaser1 = new Image(wallLaser1.toString());
+
+    URL wallLaser2 = getClass().getResource("Laser/WallLaser2.png");
+    Image imagewallLaser2 = new Image(wallLaser2.toString());
+
+
+    URL wallLaser3 = getClass().getResource("Laser/WallLaser3.png");
+    Image imagewallLaser3 = new Image(wallLaser1.toString());
+
 
 
 
