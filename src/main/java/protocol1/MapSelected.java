8d397dc1926ad2c6b6833d206a3f1d273cdeddf2
Nassrin Djafari <nassrin.djafari@hotmail.de>
2022-07-02 16:59:43 +0200

    Protocol1

--- src/main/java/protocol1/MapSelected.java
+++ src/main/java/protocol1/MapSelected.java
@@ -8,9 +8,9 @@ public class MapSelected {
     public final String messageType = MessageType.mapSelected;
     public MessageBody messageBody;
 
-    public MapSelected (String[] map) {
+    public MapSelected (String map) {
         MessageBody messageBody = new MessageBody();
-        messageBody.map = map;
+        messageBody.map =map;
         this.messageBody = messageBody;
 
     }
