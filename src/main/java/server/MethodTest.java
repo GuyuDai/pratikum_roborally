da09b8266cf81d6e6f1cc0308a02022dd60d3296
Yixue.Dai <dai@cip.ifi.lmu.de>
2022-07-03 18:22:32 +0200

    temp save

--- src/main/java/server/MethodTest.java
+++ src/main/java/server/MethodTest.java
@@ -1,10 +1,13 @@
 package server;
 
+import com.google.gson.Gson;
 import protocol.Welcome;
+import server.BoardTypes.DizzyHighway;
 
 public class MethodTest {
 
   public static void main(String[] args) {
-
+    DizzyHighway map = new DizzyHighway();
+    System.out.println(new Gson().toJson(map));
   }
 }
