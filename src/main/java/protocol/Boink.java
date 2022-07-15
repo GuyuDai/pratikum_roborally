48df110b6b47498fcbf96b4a74d22968fdf92f83
Nik <nissln@gitlab2.cip.ifi.lmu.de>
2022-07-15 16:54:47 +0200

    Extended the Protocol accordingly with Boink, CheckPointMoved, ChooseRegister, DiscardSome and RegisterChosen

--- src/main/java/protocol/Boink.java
+++ src/main/java/protocol/Boink.java
@@ -1,5 +1,21 @@
 package protocol;
 
-public class Boink {
+import protocol.ProtocolFormat.*;
+
+public class Boink extends Message{
+    public class BoinkBody extends MessageBody{
+        protected String orientation;
+
+        public String getOrientation(){
+            return orientation;
+        }
+    }
+
+    public Boink (String orientation){
+        this.messageType = MessageType.boink;
+        Boink.BoinkBody body = new Boink.BoinkBody();
+        body.orientation = orientation;
+        this.messageBody = body.toString();
+    }
 
 }
