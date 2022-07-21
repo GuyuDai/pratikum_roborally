45ad84aadb1b8066a4c3583c4a55d43c09343ccd
Felicia Saruba <felicia.saruba@gmx.de>
2022-07-21 22:33:59 +0200

    GUI changes

--- src/main/java/client/lobbyWindow/LobbyViewModel.java
+++ src/main/java/client/lobbyWindow/LobbyViewModel.java
@@ -55,36 +55,25 @@ public class LobbyViewModel {
     private Button sendBtn;
     @FXML
     private Button selectMap;
-    @FXML
-    private Label yourBotText;
-    @FXML
-    private ImageView yourRobot;
 
     /**
      * other players mat
      */
     @FXML
-    ImageView playerBot1, playerBot2, playerBot3,playerBot4, playerBot5, playerBot6;
+    ImageView playerBot1, playerBot2, playerBot3, playerBot4, playerBot5, playerBot6;
     @FXML
-    Label playerName1, playerName2, playerName3, playerName4, playerName5, playerName6 ;
+    Label playerName1, playerName2, playerName3, playerName4, playerName5, playerName6;
     @FXML
     private Label yourBotText;
     @FXML
     private ImageView yourRobot;
 
-    /**
-     * other players mat
-     */
     @FXML
     private Label yourRobotText;
     @FXML
     private Label otherRobotText;
     @FXML
-    ImageView playerBot1, playerBot2, playerBot3,playerBot4, playerBot5, playerBot6;
-    @FXML
-    ImageView box1,box2;
-    @FXML
-    Label playerName1, playerName2, playerName3, playerName4, playerName5, playerName6 ;
+    ImageView box1, box2;
 
 
     public void initialize() {
@@ -139,7 +128,7 @@ public class LobbyViewModel {
         model.addNewListItem(message);
     }
 
-    public void openGameWindow(){
+    public void openGameWindow() {
         try {
             FXMLLoader fxmlLoaderGame = new FXMLLoader(getClass().getResource("/views/Game.fxml"));
             Parent rootGame = (Parent) fxmlLoaderGame.load();
@@ -147,7 +136,7 @@ public class LobbyViewModel {
             stageGame.setTitle("Dizzy Highway");
             stageGame.setScene(new Scene(rootGame));
             stageGame.show();
-        } catch (Exception e){
+        } catch (Exception e) {
         }
     }
 
@@ -166,7 +155,7 @@ public class LobbyViewModel {
      * send messages using keyboard "Enter" key
      */
     @FXML
-    public void keyboardAction(KeyEvent keyEvent) throws IOException{
+    public void keyboardAction(KeyEvent keyEvent) throws IOException {
         if (keyEvent.getCode() == KeyCode.ENTER) {
             sendButtonAction(null);
         }
@@ -175,16 +164,16 @@ public class LobbyViewModel {
     /**
      * checks if it is a direct message or a message for all
      */
-    public void checkInput(String message){
+    public void checkInput(String message) {
         String chatToSend = "";
 
-        if(message.startsWith("@")) {
+        if (message.startsWith("@")) {
             chatToSend = createDirectMessage(message);
 
-        }else {
+        } else {
             chatToSend = createMessage(message);
         }
-        if(!chatToSend.isEmpty()){
+        if (!chatToSend.isEmpty()) {
             try {
                 Client.getClientReceive().getWriteOutput().write(chatToSend);
                 Client.getClientReceive().getWriteOutput().newLine();
@@ -198,25 +187,25 @@ public class LobbyViewModel {
     /**
      * message for all
      */
-    private String createMessage(String message){
-        String sendChat = new SendChat(message,-1).toString();
+    private String createMessage(String message) {
+        String sendChat = new SendChat(message, -1).toString();
         return sendChat;
     }
 
     /**
      * direct message
      */
-    private String createDirectMessage(String message){
+    private String createDirectMessage(String message) {
         message = message.replace("@", "");
-        String [] splittingTarget = message.split(" ");
+        String[] splittingTarget = message.split(" ");
         StringBuilder realMessage = new StringBuilder("");
-        for(int i = 1; i < splittingTarget.length; i++){
+        for (int i = 1; i < splittingTarget.length; i++) {
             realMessage.append(splittingTarget[i]).append(" ");
         }
-        String target=splittingTarget[0].trim();
-        int to=Client.getClientReceive().getIdByName(target);
+        String target = splittingTarget[0].trim();
+        int to = Client.getClientReceive().getIdByName(target);
         String messageToSend = realMessage.toString().trim();
-        String sendChat = new SendChat("(private) " + messageToSend,to).toString();
+        String sendChat = new SendChat("(private) " + messageToSend, to).toString();
 
         return sendChat;
     }
@@ -224,7 +213,8 @@ public class LobbyViewModel {
     /**
      * click on ready Button -> first player to click will select map
      */
-    int clickCount=0;
+    int clickCount = 0;
+
     public void readyButtonAction(ActionEvent actionEvent) {
         clickCount++;
         if (clickCount == 1) {
@@ -236,176 +226,162 @@ public class LobbyViewModel {
                 selectMap.setText("OPEN GAME");
                 selectMap.setVisible(true);
             }
-           /* if (readyButton.isSelected()) {
-                System.out.println("ok");
+            if (readyButton.isSelected()) {
+                System.out.println("OK");
             } else {
-                LobbyText.setText("hui");
+                LobbyText.setText("HUI");
             }
-
-            */
-        String readyMessage=new SetStatus(true).toString();
-        Client.getClientReceive().sendMessage(readyMessage);
-        if(Client.getClientReceive().getReadyList().size()<1) {
-            selectMap.setVisible(true);
-        }
-        else{
-            selectMap.setText("OPEN GAME");
-            selectMap.setVisible(true);
-        }
-        if(readyButton.isSelected()){
-            System.out.println("OK");
-        }
-        else{
-            LobbyText.setText("HUI");
-        }
-        if(clickCount == 2){
-            String notReadyMessage = new SetStatus(false).toString();
-            Client.getClientReceive().sendMessage(notReadyMessage);
-            selectMap.setVisible(false);
-            Client.getClientReceive().setMaps(null);
-            clickCount=0;
+            if (clickCount == 2) {
+                String notReadyMessage = new SetStatus(false).toString();
+                Client.getClientReceive().sendMessage(notReadyMessage);
+                selectMap.setVisible(false);
+                Client.getClientReceive().setMaps(null);
+                clickCount = 0;
+            }
         }
     }
 
-    /**
-     * selecting a map by opening map selection window, when at least 2 players are online
-     */
-    public void selectMapAction(ActionEvent actionEvent) {
-        if (Client.getClientReceive().getReadyList().size() == 1) {
-            LobbyText.setVisible(true);
-            PauseTransition pauseTransition26 = new PauseTransition(Duration.seconds(4));
-            pauseTransition26.setOnFinished(e -> LobbyText.setVisible(false));
-            pauseTransition26.play();
-        } else {
-            LobbyText.setVisible(false);
-            if (Client.getClientReceive().getMaps() != null) {
-                try {
-                    FXMLLoader fxmlLoaderGame = new FXMLLoader(getClass().getResource("/views/Map.fxml"));
-                    Parent rootGame = (Parent) fxmlLoaderGame.load();
-                    Stage stageGame = new Stage();
-                    stageGame.setTitle("Map Selection");
-                    stageGame.setScene(new Scene(rootGame));
-                    stageGame.show();
-                } catch (Exception e) {
-
-                }
-
+        /**
+         * selecting a map by opening map selection window, when at least 2 players are online
+         */
+        public void selectMapAction (ActionEvent actionEvent){
+            if (Client.getClientReceive().getReadyList().size() == 1) {
+                LobbyText.setVisible(true);
+                PauseTransition pauseTransition26 = new PauseTransition(Duration.seconds(4));
+                pauseTransition26.setOnFinished(e -> LobbyText.setVisible(false));
+                pauseTransition26.play();
             } else {
-                openGameWindow();
-            }
-            //close Lobby
-            Stage stage = (Stage) selectMap.getScene().getWindow();
-            stage.close();
-            setWindowName("Game");
-        }
-    }
+                LobbyText.setVisible(false);
+                if (Client.getClientReceive().getMaps() != null) {
+                    try {
+                        FXMLLoader fxmlLoaderGame = new FXMLLoader(getClass().getResource("/views/Map.fxml"));
+                        Parent rootGame = (Parent) fxmlLoaderGame.load();
+                        Stage stageGame = new Stage();
+                        stageGame.setTitle("Map Selection");
+                        stageGame.setScene(new Scene(rootGame));
+                        stageGame.show();
+                    } catch (Exception e) {
 
+                    }
 
-    public void setYourBotIcon(){
-        yourRobotText.setVisible(true);
-        yourBotText.setVisible(true);
-        int yourId = Client.getClientReceive().getClientID();
-        String yourName = Client.getClientReceive().getNameById(yourId);
-        int yourRobotNumber = Client.getClientReceive().getRobotById(yourId);
-        Image robotIcon = null;
-        //int aiID=AIClient.getAiClientReceive().getClientID();
-        switch (yourRobotNumber) {
-            case 1:
-                robotIcon = imageHulk;//hulk
-                break;
-            case 2:
-                robotIcon = imageSpin;//spin
-                break;
-            case 3:
-                robotIcon = imageSquash;//squash
-                break;
-            case 4:
-                robotIcon = imageHammer;//hammer
-                break;
-            case 5:
-                robotIcon = imageTwonkey;//twonkey
-                break;
-            case 6:
-                robotIcon = imageTwitch;//twitch
-                break;
-        }
-        yourRobot.setImage(robotIcon);
-        yourBotText.setText(yourName);
-        box1.setVisible(true);
-    }
-
-
-    /**
-     * Set other player´s icon and name on board.
-     */
-    public void setOthersBotIcon() {
-        for (int id : Client.getClientReceive().getIdRobot().keySet()) {
-            if (id != Client.getClientReceive().getClientID()) {
-                int otherRobotNumber = Client.getClientReceive().getIdRobot().get(id);
-                String playerName = Client.getClientReceive().getNameById(id);
-                Image otherRobotIcon = null;
-                switch (otherRobotNumber) {
-                    case 1:
-                        otherRobotIcon = imageHulk;
-                        break;
-                    case 2:
-                        otherRobotIcon = imageSpin;
-                        break;
-                    case 3:
-                        otherRobotIcon = imageSquash;
-                        break;
-                    case 4:
-                        otherRobotIcon = imageHammer;
-                        break;
-                    case 5:
-                        otherRobotIcon = imageTwonkey;
-                        break;
-                    case 6:
-                        otherRobotIcon = imageTwitch;
-                        break;
+                } else {
+                    openGameWindow();
                 }
-
-                switch (otherRobotNumber) {
-                    case 1:
-                        playerBot1.setImage(otherRobotIcon);
-                        playerName1.setText(playerName);
-                        break;
-                    case 2:
-                        playerBot2.setImage(otherRobotIcon);
-                        playerName2.setText(playerName);
-                        break;
-                    case 3:
-                        playerBot3.setImage(otherRobotIcon);
-                        playerName3.setText(playerName);
-                        break;
-                    case 4:
-                        playerBot4.setImage(otherRobotIcon);
-                        playerName4.setText(playerName);
-                        break;
-                    case 5:
-                        playerBot5.setImage(otherRobotIcon);
-                        playerName5.setText(playerName);
-                        break;
-                    case 6:
-                        playerBot6.setImage(otherRobotIcon);
-                        playerName6.setText(playerName);
-                        break;
-                }
-                otherRobotText.setVisible(true);
-                box2.setVisible(true);
+                //close Lobby
+                Stage stage = (Stage) selectMap.getScene().getWindow();
+                stage.close();
+                setWindowName("Game");
             }
         }
-    }
+
+
+        public void setYourBotIcon () {
+            yourRobotText.setVisible(true);
+            yourBotText.setVisible(true);
+            int yourId = Client.getClientReceive().getClientID();
+            String yourName = Client.getClientReceive().getNameById(yourId);
+            int yourRobotNumber = Client.getClientReceive().getRobotById(yourId);
+            Image robotIcon = null;
+            //int aiID=AIClient.getAiClientReceive().getClientID();
+            switch (yourRobotNumber) {
+                case 1:
+                    robotIcon = imageHulk;//hulk
+                    break;
+                case 2:
+                    robotIcon = imageSpin;//spin
+                    break;
+                case 3:
+                    robotIcon = imageSquash;//squash
+                    break;
+                case 4:
+                    robotIcon = imageHammer;//hammer
+                    break;
+                case 5:
+                    robotIcon = imageTwonkey;//twonkey
+                    break;
+                case 6:
+                    robotIcon = imageTwitch;//twitch
+                    break;
+            }
+            yourRobot.setImage(robotIcon);
+            yourBotText.setText(yourName);
+            box1.setVisible(true);
+        }
+
+
+        /**
+         * Set other player´s icon and name on board.
+         */
+        public void setOthersBotIcon () {
+            for (int id : Client.getClientReceive().getIdRobot().keySet()) {
+                if (id != Client.getClientReceive().getClientID()) {
+                    int otherRobotNumber = Client.getClientReceive().getIdRobot().get(id);
+                    String playerName = Client.getClientReceive().getNameById(id);
+                    Image otherRobotIcon = null;
+                    switch (otherRobotNumber) {
+                        case 1:
+                            otherRobotIcon = imageHulk;
+                            break;
+                        case 2:
+                            otherRobotIcon = imageSpin;
+                            break;
+                        case 3:
+                            otherRobotIcon = imageSquash;
+                            break;
+                        case 4:
+                            otherRobotIcon = imageHammer;
+                            break;
+                        case 5:
+                            otherRobotIcon = imageTwonkey;
+                            break;
+                        case 6:
+                            otherRobotIcon = imageTwitch;
+                            break;
+                    }
+
+                    switch (otherRobotNumber) {
+                        case 1:
+                            playerBot1.setImage(otherRobotIcon);
+                            playerName1.setText(playerName);
+                            break;
+                        case 2:
+                            playerBot2.setImage(otherRobotIcon);
+                            playerName2.setText(playerName);
+                            break;
+                        case 3:
+                            playerBot3.setImage(otherRobotIcon);
+                            playerName3.setText(playerName);
+                            break;
+                        case 4:
+                            playerBot4.setImage(otherRobotIcon);
+                            playerName4.setText(playerName);
+                            break;
+                        case 5:
+                            playerBot5.setImage(otherRobotIcon);
+                            playerName5.setText(playerName);
+                            break;
+                        case 6:
+                            playerBot6.setImage(otherRobotIcon);
+                            playerName6.setText(playerName);
+                            break;
+                    }
+                    otherRobotText.setVisible(true);
+                    box2.setVisible(true);
+                }
+            }
+        }
 
 
     public void MouseAction(MouseEvent mouseEvent) {
         setYourBotIcon();
         setOthersBotIcon();
     }
+
     public static void setWindowName (String name){
         window = name;
     }
-    public static String getWindowName(){
+
+    public static String getWindowName () {
         return window;
     }
 }
